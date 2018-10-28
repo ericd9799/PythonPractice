d9b9b7d50cfe0bc275e8c01f5a884b3dee40f69e
ericd9799 <ediep92@gmail.com>
2018-10-28 18:02:37 -0400

    complete guess number

---
+++ guessNumber.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+import random
+
+sysGenNum = random.randint(0, 10)
+print(type(sysGenNum))
+guess = int(input("Input guess (between 0 and 10): "))
+
+while guess !=  sysGenNum:
+	if guess > sysGenNum:
+		print("Guess is too high")
+		guess = int(input("Input guess (between 0 and 10): "))
+	elif guess < sysGenNum:
+		print("Guess is too low")
+		guess = int(input("Input guess (between 0 and 10): "))
+	elif guess == sysGenNum:
+		print("You are correct!")
+
+if guess == sysGenNum:
+	print("You are correct!")
