37bbda80770e17b4fb51dc11538491a7338deda9
Eric <ediep92@gmail.com>
2018-10-29 13:21:19 -0400

    Add modular check logic

--- modCheck.py
+++ modCheck.py
@@ -1,2 +1,8 @@
 #! /usr/bin/python3
 
+modCheck = int(input("Please enter an integer: "))
+
+if (modCheck % 2) == 0 :
+  print(str(modCheck) + " is even")
+elif (modCheck % 2) != 0:
+  print(str(modCheck) + " is odd")
