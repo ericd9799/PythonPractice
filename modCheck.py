653567e28cffc231b141a8f0d9089015f486b600
Eric <ediep92@gmail.com>
2018-10-29 14:12:13 -0400

    Add multiple 4 check

--- modCheck.py
+++ modCheck.py
@@ -2,7 +2,9 @@
 
 modCheck = int(input("Please enter an integer: "))
 
-if (modCheck % 2) == 0 :
+if (modCheck % 4) == 0:
+  print(str(modCheck) + " is a multiple of 4")
+elif (modCheck % 2) == 0:
   print(str(modCheck) + " is even")
 elif (modCheck % 2) != 0:
   print(str(modCheck) + " is odd")
