9c98870a77b2219eda9933390c76a8779d7754ef
Eric <ediep92@gmail.com>
2018-10-29 13:07:02 -0400

    Add name input

--- year100.py
+++ year100.py
@@ -4,10 +4,12 @@ import datetime
 
 now = datetime.datetime.now()
 year = now.year
+
+name = input("Please enter your name: ")
 age = int(input("Please enter your age: "))
 
 yearsToHundred = 100 - age
 turnHundred = int(year) + yearsToHundred
 
-print("You will turn 100 in the year "+ str(turnHundred))
+print(name + " will turn 100 in the year "+ str(turnHundred))
 
