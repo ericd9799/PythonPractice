eb87c6bf69a14c9c0b74278c842b48dd29a1e67a
Eric <ediep92@gmail.com>
2018-10-29 13:03:59 -0400

    Create year100.py

---
+++ year100.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+import datetime
+
+now = datetime.datetime.now()
+year = now.year
+age = int(input("Please enter your age: "))
+
+yearsToHundred = 100 - age
+turnHundred = year + yearsToHundred
+
+print("You will turn 100 in the year "+ turnHundred)
+
