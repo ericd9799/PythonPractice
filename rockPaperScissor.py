d16d041c48a0e07ccc4bdc2971a3cda47b94f74c
ericd9799 <ediep92@gmail.com>
2018-11-01 19:04:48 -0400

    complete  compare logic

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,19 +1,57 @@
 #! /usr/bin/python3
 
 class player():
+	choice = ""
 	def __init__(self, name):
-		self.name = name
+		self.name = name	
 	
-	def options(self, opt):
-		self.option = opt
+	def setChoice(self, choice):
+		'''Need to work on error handling'''
+		#if choice in ["R", "P", "S"]:
+		self.choice = choice
+		#else:
+			#print(choice + " is not a valid option.")
+			#self.choice = print (self.name + " enter (R)ock, (P)aper, (S)cissor: ")
 
 	def __str__(self):
-		return self.name + " selected "+ self.option
+		return self.name + " selected "+self.choice
+
+	def getName(self):
+		return self.name
+
+	def getChoice(self):
+		return self.choice
 def main():
+	won = "N"
 	player1 = player(input("Enter name for player 1: "))
 	player2 = player(input("Enter name for player 2: "))
-	player1.options(input("Enter (R)ock, (P)aper, (S)cissor: "))
-	print(player1)
+		
+	player1.setChoice(input(player1.getName() + " enter (R)ock, (P)aper, (S)cissor: "))
+	player2.setChoice(input(player2.getName() + " enter (R)ock, (P)aper, (S)cissor: "))
+	#print(player1)
+	#print(player2)
+
+	while won != "Y":
+		if player1.getChoice() == player2.getChoice():
+			print ("Tie!")
+		elif player1.getChoice() == "R":
+			if player2.getChoice() == "S":
+				print(player1.getName() + " wins!")
+				won = "Y"
+			else:
+				print(player2.getName() + " wins!")
+				won = "Y"
+		elif player1.getChoice() == "S":
+			if player2.getChoice() == "P":
+				print(player1.getName() + " wins!")
+			else:
+				print(player2.getName() + " wins!")
+		elif player1.getChoice() == "P":
+			if player2.getChoice() == "R":
+				print(player1.getName() + " wins!")
+			else:
+				print(player2.getName() + " wins!")	
+	
 
 if __name__ == "__main__":
 	main()
