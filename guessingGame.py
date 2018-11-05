4701091a9c4cb1d2dfe5b1a28f07be8fa9ef418f
ericd9799 <ediep92@gmail.com>
2018-11-05 06:53:39 -0500

    working guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -3,29 +3,26 @@
 import random
 
 def main():
-  genNum = random.randint(1, 9)
-  guessCnt = 0
-  correct = "N"
+
+	guessCnt = 0
+	genNum = random.randint(1, 9)
   
-  while correct != "Y":
-    guess = int(input("Please enter a guess between 1 - 9: "))
-    guessCnt = guessCnt + 1
-  
-    if guess > genNum:
-      print("Your guess is too high")
-      correct = "N"
-    elif guess < genNum:
-      print("Your guess is too low")
-      correct = "N"
-    elif guess == genNum:
-      print("You are correct! You took "+ str(guessCnt) + " guess(es).")
-      correct = "Y"
-  
-  if correct = "Y":
-     if input("Please enter Y to play again: ") == "Y":
-        main()
-     else:
-        break
-  
- if __name__ = "__main__":
-    main()
+	while True:
+		guess = int(input("Enter guess between 1 - 9: "))
+		guessCnt += 1
+	
+		if guess < genNum:
+			print ("Too low!")
+			continue
+		elif guess > genNum:
+			print ("Too high!")
+			continue
+		elif guess == genNum:
+			print ("Correct! You took "+ str(guessCnt) + " attempts!")
+			if input("Enter Y to continue: ").upper() == "Y":
+				continue
+			else:
+				break
+
+if __name__ == "__main__":
+	main()
