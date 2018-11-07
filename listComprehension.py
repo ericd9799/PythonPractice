3152004365aefa65379b58f529ef0a3d4dbb5cad
ericd9799 <ediep92@gmail.com>
2018-11-07 06:54:13 -0500

    "Compare logic work. Add random list"

--- listComprehension.py
+++ listComprehension.py
@@ -1,7 +1,13 @@
 #! /usr/bin/python3
 
-def main(a, b):
-	both = [ itemOne for itemOne in a for itemTwo in b if itemOne == itemTwo]
+def listOverlap(a, b):
+	both = [ itemOne for itemOne in set(a) for itemTwo in b if itemOne == itemTwo]
+	# not necessary to perform inner for-loop to iterate through list
+	# could have used in-clause since it is list
+	return both
 
-if __name__ = "__main__":
+def main():
+	print(listOverlap([1,1,2,3,5,6,7,8],[1,2,3,4,5,6,7,8]))
+
+if __name__ == "__main__":
 	main()
