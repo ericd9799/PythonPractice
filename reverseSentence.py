ad976db82e3ee9719fe1a8f0ca92ad0ee7b99a03
Eric <ediep92@gmail.com>
2018-11-07 12:47:33 -0500

    Add sentence reverse logic

--- reverseSentence.py
+++ reverseSentence.py
@@ -1,1 +1,10 @@
+#! usr/bin/python3
 
+def reverseString(sentence):
+	return sentence.split()[::-1]
+
+def main():
+	print(reverseString(input('Please enter a setence: ')))
+
+if __name__ == '__main__':
+	main()
