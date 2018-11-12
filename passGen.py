a17c0c18dbf4bb55af74ed6784c404348db1d4c2
Eric <ediep92@gmail.com>
2018-11-12 13:50:22 -0500

    call string constant

--- passGen.py
+++ passGen.py
@@ -1,1 +1,9 @@
+#! /usr/bin/python3
 
+import random
+import string
+
+alphaUpper = string.ascii_uppercase
+alphaLower = string.ascii_lowercase
+digit = string.digits
+punctuation = string.punctuation
