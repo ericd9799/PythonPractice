5e29fa3e91a1117043e711a88d13ec2a48ee862f
Eric <ediep92@gmail.com>
2018-11-12 13:28:57 -0500

    Add second diagonal check

--- tictactoe.py
+++ tictactoe.py
@@ -32,7 +32,7 @@ def win_check(board, mark):
 	# 3 in horizontal, 3 in diagonal, 3 in vertical
 	if (board[1] == board[2] == board[3] == mark) or (board[4] == board[5] == board[6]  == mark) or (board[7] == board[8] == board[9] == mark):
 		return True
-	elif (board[1] == board[5] == board[9] == mark):
+	elif (board[1] == board[5] == board[9] == mark) or (board[3] == board[5] == board[7] == mark):
 		return True
 	elif (board[1] == board[4] == board[7] == mark) or (board[2] == board[5] == board[8]  == mark) or (board[3] == board[6] == board[9] == mark):
 		return True
