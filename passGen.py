1c31a93ef5f654484c435e93c6ca33c454a69220
ericd9799 <ediep92@gmail.com>
2018-11-27 18:32:44 -0500

    add main function

--- passGen.py
+++ passGen.py
@@ -7,3 +7,21 @@ alphaUpper = string.ascii_uppercase
 alphaLower = string.ascii_lowercase
 digit = string.digits
 punctuation = string.punctuation
+
+def userInput():
+	check = True
+	while check==True:
+		strength = input("Enter strength of password (weak, medium, strong): ")
+		if strength.lower() in ('weak', 'medium', 'strong'):
+			return strength.lower()
+			check = False
+		else:
+			check = True
+
+def main():
+	passStrength = userInput()
+	print("Password strength: {}".format(passStrength))
+	
+
+if __name__ == '__main__':
+	main()
