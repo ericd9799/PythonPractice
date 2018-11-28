58c876312cd48d965cd46305aca5b117461e4304
ericd9799 <ediep92@gmail.com>
2018-11-28 18:54:02 -0500

    add initial logic

--- passGen.py
+++ passGen.py
@@ -6,7 +6,7 @@ import string
 alphaUpper = string.ascii_uppercase
 alphaLower = string.ascii_lowercase
 digit = string.digits
-punctuation = string.punctuation
+punctuation = '!@#$%.'
 
 def userInput():
 	check = True
@@ -20,8 +20,19 @@ def userInput():
 
 def main():
 	passStrength = userInput()
-	print("Password strength: {}".format(passStrength))
-	
+	password = ''
+	for i in range(0, 10):
+		key = random.randrange(0, 4)
+		if key == 0:
+			#alphUpper
+			password += random.choice(alphaUpper)
+		elif key == 1:
+			password += random.choice(alphaLower)
+		elif key == 2:
+			password += random.choice(digit)
+		else:
+			password += random.choice(punctuation)
+	print(password)
 
 if __name__ == '__main__':
 	main()
